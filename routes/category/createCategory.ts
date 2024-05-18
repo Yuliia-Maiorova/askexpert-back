1b2ec86dec65d0eb56c3c2b8656d0502f5f782fa
Thibault CASTILLON <thibault.castillon@epitech.eu>
2024-05-18 23:35:10 +0100

    [ADD] createCategory

---
+++ routes/category/createCategory.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import Categories from '../../models/categories.model';
+
+async function createCategory(req: Request, res: Response) {
+    try {
+        if ((!req.body.hex_code) || (!req.body.name)) return res.status(400).json({error: 'Missing fields'})
+
+        const {hex_code, name} = req.body;
+
+        // create a new category
+
+        const category = await Categories.create(req.body)
+
+        return res.status(201).json(category)
+    } catch (e: any) {
+        return res.status(500).json({error: e.message})
+    }
+}
+
+export default createCategory;
